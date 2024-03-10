8408aa4c15d81156fa9a6868ebae370362f84b7e
jweaver9 <161187647+jweaver9@users.noreply.github.com>
2024-03-09 20:23:32 -0600

    Create InteractivePart.tsx

---
+++ app/InteractivePart.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+function InteractivePart() {
+    const [state, setState] = useState();
+
+    // Your client-side interactive logic
+
+    return (
+        // Your JSX
+    );
+}
+
+/* use client */
+export default InteractivePart;
